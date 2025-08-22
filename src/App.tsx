99f4c4ae9e2d553d325b58b59540724e4bb91426
Parker Swinton <parkerdswinton@gmail.com>
2025-08-21 17:29:12 -0700

    feat: add draggable wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { DndContext, type DragEndEvent } from "@dnd-kit/core";
+import { Draggable } from "./components/Draggable";
+
 export const App = () => {
+  const handleDragEnd = (e: DragEndEvent) => {
+    console.log(e);
+  };
+
   return (
-    <div>
-      <h1 className="font-dseg text-4xl">0123456789</h1>
+    <div className="h-screen w-screen overflow-hidden">
+      <DndContext onDragEnd={handleDragEnd}>
+        <Draggable>
+          <h1 className="font-dseg text-4xl">0123456789</h1>
+        </Draggable>
+      </DndContext>
     </div>
   );
 };
---
+++ src/components/Draggable.tsx
@@ -0,0 +1,19 @@
+import { useDraggable } from "@dnd-kit/core";
+import type { ReactNode } from "react";
+
+export const Draggable = ({ children }: { children: ReactNode }) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: "draggable",
+  });
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
+
+  return (
+    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {children}
+    </div>
+  );
+};
